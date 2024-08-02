efc4c05382825edb2cb3bdb79d7189aa068cd5c9
Lyinceer <theyxeldev113@gmail.com>
2024-08-02 21:37:11 +0700

    Updated twrp_a55x.mk

--- twrp_a55x.mk
+++ twrp_a55x.mk
@@ -6,18 +6,8 @@
 
 DEVICE_PATH := device/samsung/a55x
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Inherit from a55x device
-$(call inherit-product, device/samsung/a55x/device.mk)
-
-# Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Product Name
 PRODUCT_RELEASE_NAME := a55x
