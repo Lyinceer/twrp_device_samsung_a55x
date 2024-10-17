819cebab155c61971ab834ac1ded66153b85a057
Lyinceer <theyxeldev113@gmail.com>
2024-10-17 20:27:34 +0700

    [ Changed and added some flags ]

--- twrp_a55x.mk
+++ twrp_a55x.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
