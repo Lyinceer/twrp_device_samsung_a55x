aaf25b1361cfe09830de82ae46bf0cd3be1d093f
Lyinceer <theyxeldev113@gmail.com>
2025-03-07 22:06:08 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,7 +123,6 @@ TARGET_BOARD_PLATFORM := erd8845
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.recovery.s5e8845.rc
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 # Recovery
